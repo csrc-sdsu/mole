860168642e54bf4f14620ce119dc6fdf7b2bc284
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2021-08-01 13:08:48 -0700

    Update elliptic1D.cpp

--- examples_C++/elliptic1D.cpp
+++ examples_C++/elliptic1D.cpp
@@ -37,8 +37,8 @@ int main() {
     rhs(m+1) = 2*exp(1);  // rhs(1) = 2e
 
     // Solve the system of linear equations
-    // Use Eigen only if SuperLU (faster) is not available
 #ifdef EIGEN
+    // Use Eigen only if SuperLU (faster) is not available
     vec sol = Utils::spsolve_eigen(L, rhs);
 #else
     vec sol = spsolve(L, rhs); // Will use SuperLU
