97dbc82c8f01ccd388439e8c7e0e12fc815dc951
Miguel Dumett <mdumett@sdsu.edu>
2023-02-11 17:57:50 -0800

    some filename typos

--- mole_MATLAB/interpolNodesToCenters2D.m
+++ mole_MATLAB/interpolNodesToCenters2D.m
@@ -1,6 +1,6 @@
 function I = interpolNodesToCenters2D(k, m, n)
 % interpolation operator from nodal coordinates to staggered centers
-% m, o, are the number of cells in the logical x-, y- axes
+% m, n, are the number of cells in the logical x-, y- axes
 % nodal logical coordinates are [1:1:m]x[1:1:n]
 % centers logical coordinates [1,1.5:m-0.5,m]x[1,1.5:n-0.5,n]
 
