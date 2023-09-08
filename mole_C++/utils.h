aee181e70c1b78450d3bb59c7198f19312d6eb66
Jared Brzenski <jbrzenski@sdsu.edu>
2023-09-08 11:26:58 -0700

    MATLAB-like Meshgrid functionality added to C++ library

--- mole_C++/utils.h
+++ mole_C++/utils.h
@@ -15,6 +15,9 @@ public:
     static sp_mat spjoin_rows(const sp_mat &A, const sp_mat &B);
     static sp_mat spjoin_cols(const sp_mat &A, const sp_mat &B);
     static vec spsolve_eigen(const sp_mat &A, const vec &b);
+    // MATLAB-like MeshGrid
+    void meshgrid( const vec &x, const vec &y, mat &X, mat &Y);
+    void meshgrid( const vec &x, const vec &y, const vec &z, cube &X, cube &Y, cube &Z);
 };
 
 #endif // UTILS_H
