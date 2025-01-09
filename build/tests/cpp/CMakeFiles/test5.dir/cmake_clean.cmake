1459c3a5a9c0f215770baa8878480dabcf06b3ea
Janani Srinivasan <janani@Christophers-MacBook-Pro.local>
2025-01-09 14:07:51 -0800

    SuperLu and Armadillo Installation automated

---
+++ build/tests/cpp/CMakeFiles/test5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test5.dir/test5.cpp.o"
+  "CMakeFiles/test5.dir/test5.cpp.o.d"
+  "test5"
+  "test5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/third_party_build/armadillo-14.2.2/CMakeFiles/armadillo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  ".14"
+  "CMakeFiles/armadillo.dir/src/wrapper1.cpp.o"
+  "CMakeFiles/armadillo.dir/src/wrapper1.cpp.o.d"
+  "CMakeFiles/armadillo.dir/src/wrapper2.cpp.o"
+  "CMakeFiles/armadillo.dir/src/wrapper2.cpp.o.d"
+  "libarmadillo.14.2.2.dylib"
+  "libarmadillo.14.dylib"
+  "libarmadillo.dylib"
+  "libarmadillo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armadillo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
