1ff620c5627b87f525f20c620bfe005081e242ab
JananiPSrinivasan <jsrinivasan4020@sdsu.edu>
2025-01-09 20:04:36 -0800

    Revert "SuperLu and Armadillo Installation automated"
    This reverts commit ef5c4be2a52836fe7fef933243de7e87692e5070.

--- build/third_party_build/armadillo-14.2.2/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/janani/mole/build/third_party_build/armadillo-14.2.2
-# Build directory: /Users/janani/mole/build/third_party_build/armadillo-14.2.2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests1")
