c1c679462d1aaf79fd05246f26a0e3e9404faa20
Janani Srinivasan <janani@Christophers-MacBook-Pro.local>
2025-01-09 18:31:31 -0800

    CMakeLists.txt updated

--- build/CMakeFiles/all_build.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/all_build"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/all_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
