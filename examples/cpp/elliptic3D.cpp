6c22a788b7b7b86dfbca4559b3f84a610bf6d186
Katayoon <kaviany.katayoon@gmail.com>
2025-02-22 15:12:48 -0800

    Update elliptic3D and Makefile after first review

--- examples/cpp/elliptic3D.cpp
+++ examples/cpp/elliptic3D.cpp
@@ -3,38 +3,47 @@
  * It is the equivalent to examples_MATLAB/elliptic3D.m
  */
 
-#include "mole.h"
 #include <iostream>
+#include "mole.h"
 
 int main() {
-  int k = 2; // Operators' order of accuracy
-  int m = 5; // Vertical resolution
-  int n = 6; // Horizontal resolution
-  int p = 7; // Depth resolution
+  constexpr int k = 2;  // Operators' order of accuracy
+  constexpr int m = 5;  // Vertical resolution
+  constexpr int n = 6;  // Horizontal resolution
+  constexpr int p = 7;  // Depth resolution
+
+  // Grid spacing in the x, y, and z directions
+  constexpr double dx = 1.0;
+  constexpr double dy = 1.0;
+  constexpr double dz = 1.0;
 
   // Get mimetic operators
-  Laplacian L(k, m, n, p, 1, 1, 1);
-  RobinBC BC(k, m, 1, n, 1, p, 1, 1, 0); // Dirichlet BC
+  Laplacian L(k, m, n, p, dx, dy, dz);
+  RobinBC BC(k, m, dx, n, dy, p, dz, 1, 0);  // Dirichlet BC
   L = L + BC;
 
   // Build RHS for system of equations
-  cube rhs(m + 2, n + 2, p + 2, fill::zeros);
-//   rhs.slice(0) = 100 * ones(n + 2, p + 2); // Known value at the bottom boundary
-  rhs.slice(0).fill(100); 
+  arma::cube rhs(m + 2, n + 2, p + 2, arma::fill::zeros);
+  rhs.slice(0).fill(100);
 
-  // Solve the system of linear equations
-#ifdef EIGEN
-    // Use Eigen only if SuperLU (faster) is not available
-    vec sol = Utils::spsolve_eigen(L, vectorise(rhs));
+
+  arma::vec sol;  // Declare sol here
+
+#ifdef SuperLU
+  cout << "Using SuperLU solver" << endl;
+  // Use SuperLU (faster) if available
+  sol = arma::spsolve(L, arma::vectorise(rhs));  // Will use SuperLU
+#elif EIGEN
+  cout << "Using Eigen solver" << endl;
+  sol = arma::Utils::spsolve_eigen(L, arma::vectorise(rhs));
 #else
-    vec sol = spsolve(L, vectorise(rhs)); // Will use SuperLU
+  cerr << "Error: No solver available." << endl;
+  return -1;  // Exit if no solver is available
 #endif
 
-    // Print out the solution
-    // cout << reshape(sol, m + 2, n + 2, p + 2);
-    arma::cube sol_cube = arma::cube(sol.memptr(), m + 2, n + 2, p + 2);
-    cout << sol_cube;
-    
-    return 0;
-    }
+  // Print out the solution
+  arma::cube sol_cube = arma::cube(sol.memptr(), m + 2, n + 2, p + 2);
+  cout << sol_cube;
 
+  return 0;
+}
