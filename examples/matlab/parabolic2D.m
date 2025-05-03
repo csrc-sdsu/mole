4f4745bddc650edeedd1ae05d7d90f0c1da400e7
Oluchi Nzerem <ojnzerem@gmail.com>
2025-05-03 00:01:54 -0700

    parabolic 2D/3D

---
+++ examples/matlab/parabolic2D.m
@@ -0,0 +1,95 @@
+clc
+close all
+
+addpath('../../src/matlab')
+
+% Parameters
+method = "implicit";
+
+alpha = 0.1;                     %Diffusion coefficient/viscocity
+
+k = 2;                           %Order of accuracy
+
+nx = 40;                         %Number of steps in space(x)
+ny = 50;                         %Number of steps in space(y)       
+
+
+dx=2/nx;                         % Width of space step(x)
+dy=2/ny;                         % Width of space step(y)
+
+x=0:dx:2;
+y=0:dy:2;
+
+t=3;                                 % Number of time steps
+
+if method == "explicit"
+    dt=0.001;                        % Width of each time step (explicit)
+else
+    dt = 0.01;                       % Width of each time step (implicit)
+end
+
+
+U=zeros(nx+2,ny+2);              %Preallocating u
+
+
+xgrid = [a a+dx/2 : dx : b-dx/2 b];
+ygrid = [c c+dy/2 : dy : d-dy/2 d];
+
+% Initial Conditions: u(x, y, 0) = 2 
+for i=1:nx
+    for j=1:ny
+        if ((1<=y(j))&&(y(j)<=1.5)&&(1<=x(i))&&(x(i)<=1.5))
+            U(i,j)=2;
+        end
+    end
+end
+
+% set boundary conditions
+dc = [1;1;1;1];
+nc = [0;0;0;0];
+
+bcl = zeros(ny, 1);
+bcr = zeros(ny, 1);
+bct = zeros(nx+2, 1);
+bcb = zeros(nx+2, 1);
+
+v = {bcl;bcr;bcb;bct};
+
+U = reshape(U,[],1);
+
+L = lap2D(k, nx, dx, ny, dy);
+
+[L,U] = addScalarBC2D(L,U,k,nx,dx,ny,dy,dc,nc,v);
+
+U = reshape(U, nx+2, ny+2);
+
+ switch method
+    case "explicit"
+        L = alpha*dt*L + speye(size(L));   
+    case "implicit"
+        L = speye(size(L)) - alpha*dt*L;
+ end
+
+for it=0:t/dt 
+    surf(xgrid, ygrid, U')
+    shading interp
+    axis ([0 2 0 2 0 2])
+    title({[method];['2-D Diffusion with {\nu} = ',num2str(alpha)];['time (\itt) = ',num2str(it*dt)]})
+    xlabel('Spatial co-ordinate (x) \rightarrow')
+    ylabel('{\leftarrow} Spatial co-ordinate (y)')
+    zlabel('Transport property profile (u) \rightarrow')
+    drawnow; 
+
+    U = reshape(U, [], 1);
+
+    switch method
+        case "explicit"
+            U = L * U;  
+        case "implicit"
+            U = L \ U; 
+    end
+    [L,U] = addScalarBC2D(L,U,k,nx,dx,ny,dy,dc,nc,v); % add boundary conditions
+
+    U = reshape(U, nx+2, ny+2);
+
+end
---
+++ examples/matlab/parabolic3D.m
@@ -0,0 +1,104 @@
+clear; close all; clc;
+
+addpath('../../src/matlab')
+
+% copper
+tc = 401;                                       % thermal conductivity (W/m.K)
+cp = 390;                                       % specific heat (J/kg K)
+rho = 8940;                                     % density (kg/m^3) 
+
+%% Domain parameters
+method = "implicit";
+k = 2;
+
+m = 39; 
+n = 39; 
+o = 39; 
+
+dx = 2/m;
+dy = 1/n;
+dz = 1/o;
+
+xc = [0 dx/2:dx:2-dx/2 2]';
+yc = [0 dy/2:dy:1-dy/2 1]';
+zc = [0 dz/2:dz:1-dz/2 1]';
+
+[Y, X, Z] = meshgrid(yc, xc, zc);
+
+alph = tc/(cp*rho); 
+dt = 1/(2*alph*((1/dx^2)+(1/dy^2)+(1/dz^2)));
+t = dt*10;
+
+%% Initial and boundary conditions (1st type, a.k.a Dirichlet)
+
+% boundary type: Dirichlet
+dc = [1;1;1;1;1;1];
+nc = [0;0;0;0;0;0];
+
+bcl = 20 * ones(n*o, 1);              % left
+bcr = -20 * ones(n*o, 1);             % right
+bcb = zeros((m+2)*o, 1);              % bottom
+bct = zeros((m+2)*o, 1);              % top
+bcf = zeros((n+2)*(m+2), 1);          % front
+bcz = zeros((n+2)*(m+2), 1);          % back
+
+v = {bcl;bcr;bcb;bct;bcf;bcz};
+
+L = lap3D(k, m, dx, n, dy, o, dz);
+
+U = zeros(m+2,n+2,o+2);
+
+U = reshape(U, [], 1);
+
+switch method
+    case "explicit"
+        L = alph*dt*L +speye(size(L));  
+    case "implicit"
+        L = speye(size(L)) - alph*dt*L;
+end
+
+[L, U] = addScalarBC3D(L, U, k, m, dx, n, dy, o, dz, dc, nc, v); 
+
+for it = 0 : t / dt
+    switch method
+        case "explicit" 
+            U = L * U; 
+        case "implicit"
+            U = L \ U;  
+    end
+
+    [L, U] = addScalarBC3D(L, U, k, m, dx, n, dy, o, dz, dc, nc, v); 
+
+end
+
+U = reshape(U, m+2, n+2, o+2);
+
+
+ix = round((m+3)/2);  % Middle x-index
+iy = round((n+3)/2);  % Middle y-index
+iz = round((o+3)/2);  % Middle z-index
+
+figure(1)
+surf(squeeze(Y(ix,:,:)), squeeze(Z(ix,:,:)), squeeze(U(ix,:,:)));
+xlabel('y'); ylabel('z'); zlabel('U');
+title('YZ Slice');
+shading interp
+colorbar
+
+figure(2)
+surf(squeeze(X(:,iy,:)),squeeze(Z(:,iy,:)),squeeze(U(:,iy,:)));
+title('XZ slice');
+xlabel('X');
+ylabel('Z');
+shading interp;
+colorbar
+
+figure(3)
+surf(squeeze(X(:,:,iz)),squeeze(Y(:,:,iz)),squeeze(U(:,:,iz)));
+title('XY slice');
+xlabel('X');
+ylabel('Y');
+shading interp;
+colorbar
+
+
