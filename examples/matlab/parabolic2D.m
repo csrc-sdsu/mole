39beab82447a5f3b62c0b374c8069af1504f6ae6
Oluchi Nzerem <ojnzerem@gmail.com>
2025-05-12 20:28:12 -0700

    format suggestions

--- examples/matlab/parabolic2D.m
+++ examples/matlab/parabolic2D.m
@@ -1,65 +1,72 @@
+ % ================================================== 
+ % 2D Parabolic BVP: Dirchlet, Dirichlet
+ % u_t = a(u_xx + u_yy), 0 < x,y < 2, t > 0, a = 0.1 
+ % IC: u(x,y,0) = 2 in square [1,1.5] x [1,1.5], else 0
+ % BC: Dirichlet (u = 0) on all boundaries
+ % exact solution: unknown 
+ % =================================================== 
+
 clc
 close all
 
+% Add paths to lap2D and addScalarBC2D in the src/matlab
 addpath('../../src/matlab')
 
 % Parameters
-method = "explicit";
+method = "implicit";
 
-alpha = 0.1;                     %Diffusion coefficient/viscocity
+alpha = 0.1;                      % Diffusion coefficient/viscocity
 
-k = 2;                           %Order of accuracy
+k = 2;                            % Order of accuracy
 
-nx = 40;                         %Number of steps in space(x)
-ny = 50;                         %Number of steps in space(y)       
+nx = 40;                          % Number of steps in space (x)
+ny = 50;                          % Number of steps in space (y)       
 
+dx = 2/nx;                        % Width of space step (x)
 
-dx=2/nx;                         % Width of space step(x)
-dy=2/ny;                         % Width of space step(y)
+dy = 2/ny;                        % Width of space step (y)
 
-x=0:dx:2;
-y=0:dy:2;
+x = 0: dx :2;
+y = 0: dy :2;
 
-t=3;                                 % Number of time steps
+t = 3;                            % Number of time steps
 
 if method == "explicit"
-    dt=0.001;                        % Width of each time step (explicit)
+    dt = 0.001;                   % Width of each time step (explicit)
 else
-    dt = 0.01;                       % Width of each time step (implicit)
+    dt = 0.01;                    % Width of each time step (implicit)
 end
 
+U = zeros(nx+2, ny+2);             % Preallocating u
 
-U=zeros(nx+2,ny+2);              %Preallocating u
-
-
-xgrid = [0 dx/2 : dx : 2-dx/2 2];
-ygrid = [0 dy/2 : dy : 2-dy/2 2];
+xgrid = [0 dx/2: dx :2-dx/2 2];
+ygrid = [0 dy/2: dy :2-dy/2 2];
 
 % Initial Conditions: u(x, y, 0) = 2 
-for i=1:nx
-    for j=1:ny
-        if ((1<=y(j))&&(y(j)<=1.5)&&(1<=x(i))&&(x(i)<=1.5))
-            U(i,j)=2;
+for i = 1:nx
+    for j = 1:ny
+        if ((1 <= y(j)) && (y(j) <= 1.5) && (1 <= x(i)) && (x(i) <= 1.5))
+            U(i,j) = 2;
         end
     end
 end
 
 % set boundary conditions
-dc = [1;1;1;1];
-nc = [0;0;0;0];
+dc = [1; 1; 1; 1];
+nc = [0; 0; 0; 0];
 
 bcl = zeros(ny, 1);
 bcr = zeros(ny, 1);
 bct = zeros(nx+2, 1);
 bcb = zeros(nx+2, 1);
 
-v = {bcl;bcr;bcb;bct};
+v = {bcl; bcr; bcb; bct};
 
-U = reshape(U,[],1);
+U = reshape(U, [], 1);
 
 L = lap2D(k, nx, dx, ny, dy);
 
-[L,U] = addScalarBC2D(L,U,k,nx,dx,ny,dy,dc,nc,v);
+[L,U] = addScalarBC2D(L, U, k, nx, dx, ny, dy, dc, nc, v);
 
 U = reshape(U, nx+2, ny+2);
 
@@ -70,26 +77,25 @@ U = reshape(U, nx+2, ny+2);
         L = speye(size(L)) - alpha*dt*L;
  end
 
-for it=0:t/dt 
+for it = 0 : t/dt 
     surf(xgrid, ygrid, U')
     shading interp
     axis ([0 2 0 2 0 2])
     title({[method];['2-D Diffusion with {\nu} = ',num2str(alpha)];['time (\itt) = ',num2str(it*dt)]})
-    xlabel('Spatial co-ordinate (x) \rightarrow')
-    ylabel('{\leftarrow} Spatial co-ordinate (y)')
-    zlabel('Transport property profile (u) \rightarrow')
+    xlabel('x')
+    ylabel('y')
+    zlabel('u')
     drawnow; 
 
     U = reshape(U, [], 1);
 
     switch method
         case "explicit"
-            U = L * U;  
+            U = L*U;  
         case "implicit"
-            U = L \ U; 
+            U = L\U; 
     end
-    [L,U] = addScalarBC2D(L,U,k,nx,dx,ny,dy,dc,nc,v); % add boundary conditions
-
+    
     U = reshape(U, nx+2, ny+2);
 
 end
