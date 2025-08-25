8a25a2e32e4c89246f20e81a6d2cbabfe7b522f3
Joe Hellmers <jlhellmers@sdsu.edu>
2025-08-25 13:56:43 -0700

    feat: add P and Q weight generation

---
+++ src/matlab_octave/generateWeights.m
@@ -0,0 +1,38 @@
+% generates a comma-delimited file for weights used by MOLE libray
+clc
+close all
+
+% Used to specify the number of grid points to calculate weights for
+m_cnt = 25;
+decimal_positions = 9;
+
+warning('off','all');
+mkdir('../dat');
+warning('on','all')
+
+fid = fopen('../dat/pweights.csv','wt');
+for i = 1:3
+    k = 2*i;
+    j = 2*k+1;
+    m_max = j + m_cnt;
+    for m = j:m_max 
+        P = weightsP(k, m, dx);
+        out = string(k) + ',' + string(m) + ',' + strjoin(compose("%."+string(decimal_positions)+"f",P),",") + '\n';
+        fprintf(fid, out);
+    end;
+end;
+fclose(fid);
+
+fid = fopen('../dat/qweights.csv','wt');
+for i = 1:3
+    k = 2*i;
+    j = 2*k+1;
+    m_max = j + m_cnt;
+    for m = j:m_max 
+        Q = weightsQ(k, m, dx);
+        out = string(k) + ',' + string(m) + ',' + strjoin(compose("%."+string(decimal_positions)+"f",Q),",") + '\n';
+        disp(out);
+        fprintf(fid, out);
+    end;
+end;
+fclose(fid);
