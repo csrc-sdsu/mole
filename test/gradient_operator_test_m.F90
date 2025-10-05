d4b6c1ef06b4383d6f5b6a8d53fc2ab8d37e7d82
Damian Rouson <rouson@lbl.gov>
2025-10-04 21:12:15 -0400

    test(gradient): add 2nd line differentiation case

--- test/gradient_operator_test_m.F90
+++ test/gradient_operator_test_m.F90
@@ -6,7 +6,15 @@
 
 module gradient_operator_test_m
   use julienne_m, only : &
-     test_t, test_description_t, test_diagnosis_t, test_result_t, operator(.all.), operator(.approximates.), operator(.within.)
+     string_t &
+    ,test_t &
+    ,test_description_t &
+    ,test_diagnosis_t &
+    ,test_result_t &
+    ,operator(//) &
+    ,operator(.all.) &
+    ,operator(.approximates.) &
+    ,operator(.within.)
   use mole_m, only : cell_centers_extended_t, gradient_t
 #if ! HAVE_PROCEDURE_ACTUAL_FOR_POINTER_DUMMY
   use julienne_m, only : diagnosis_function_i
@@ -18,6 +26,8 @@ module gradient_operator_test_m
     procedure, nopass :: subject
     procedure, nopass :: results
   end type
+
+  double precision, parameter :: line_tolerance = 1D-14
 
 contains
 
@@ -32,7 +42,7 @@ contains
     type(gradient_operator_test_t) gradient_operator_test
     type(test_result_t), allocatable :: test_results(:)
     test_results = gradient_operator_test%run([ & 
-       test_description_t('computing the gradient of a linear function', check_gradient_of_line) &
+       test_description_t('computing gradients of lines within a tolerance of ' // string_t(line_tolerance), check_line_slope) &
     ])
   end function
 
@@ -42,22 +52,30 @@ contains
     type(gradient_operator_test_t) gradient_operator_test
     type(test_result_t), allocatable :: test_results(:)
     procedure(diagnosis_function_i), pointer :: &
-      check_gradient_of_line_ptr => check_gradient_of_line
+      check_line_slope_ptr => check_line_slope
     test_results = gradient_operator_test%run([ &
-       test_description_t('computing the gradient of a linear function', check_gradient_of_line_ptr) &
+       test_description_t('computing gradients of lines within a tolerance of ' // string_t(line_tolerance), check_line_slope_ptr) &
     ])
   end function
 
 #endif
 
-  function check_gradient_of_line() result(test_diagnosis)
+  function check_line_slope() result(test_diagnosis)
     type(test_diagnosis_t) test_diagnosis
-    double precision, parameter :: dx = 1D0, tolerance = 1D-15
-    double precision, parameter :: x(*) = [0D0,.5D0, 1.5D0, 2.5D0, 3.5D0, 4D0]*dx !! grid from Corbino & Castillo (2020) Fig. 6
-    double precision, parameter :: f(*) = x, df_dx_exact = 1D0 !! f(x) = x, df_dx = 1
-    type(gradient_t) grad_f
+    integer i
+    integer, parameter :: cells = 10
+    double precision, parameter :: x_min = 0., x_max = 10., dx = (x_max - x_min)/dble(cells)
+    double precision, parameter :: x(*) = [x_min, x_min + dx/2. + [(dble(i-1)*dx, i = 1, cells)], x_max]*dx
+      !! boundaries + grid cell centers -- see Corbino & Castillo (2020) https://doi.org/10.1016/j.cam.2019.06.042
+    double precision, parameter :: m = 2D0, b = 3D0, n = 5D0, c = 7D0
+    double precision, parameter :: f(*) = m*x + b, df_dx = m
+    double precision, parameter :: g(*) = n*x + c, dg_dx = n
+    type(gradient_t) grad_f, grad_g
     grad_f = .grad. cell_centers_extended_t(f, k=2, dx=dx) ! gfortran blocks use of association
-    test_diagnosis = .all. (grad_f%values() .approximates. df_dx_exact .within. tolerance)
+    test_diagnosis = .all. (grad_f%values() .approximates. df_dx .within. line_tolerance) // " (df_dx)"
+
+    grad_g = .grad. cell_centers_extended_t(g, k=2, dx=dx) ! gfortran blocks use of association
+    test_diagnosis = .all. (grad_g%values() .approximates. dg_dx .within. line_tolerance) // " (dg_dx)"
   end function
 
 end module
