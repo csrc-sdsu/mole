02a4f88400db857d419496b0cb2eafb82f59ca06
Damian Rouson <rouson@lbl.gov>
2025-10-07 18:01:26 -0400

    feat(cell_centers_ex): add cells_ component

--- src/fortran/cell_centers_extended_s.F90
+++ src/fortran/cell_centers_extended_s.F90
@@ -14,6 +14,7 @@ contains
 
     associate(dx => dble(x_max - x_min)/dble(cells))
       cell_centers_extended%grid_ = [x_min, x_min + dx/2. + [((cell-1)*dx, cell = 1, cells)], x_max] ! boundaries + cell centers as described in
+      cell_centers_extended%cells_ = cells
       cell_centers_extended%scalar_1D_ = scalar_1D_initializer%f(cell_centers_extended%grid_)        ! Corbino & Castillo (2020)
       cell_centers_extended%gradient_operator_ = gradient_operator_t(k=order, dx=dx, m=cells)        ! https://doi.org/10.1016/j.cam.2019.06.042
     end associate
