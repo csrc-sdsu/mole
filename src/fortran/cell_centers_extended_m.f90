aa036a831efb7976045f0fe4460f67c120696b42
Damian Rouson <rouson@lbl.gov>
2025-10-09 00:25:18 -0400

    refac(gradient_t): mv to separate module/submodule

--- src/fortran/cell_centers_extended_m.f90
+++ src/fortran/cell_centers_extended_m.f90
@@ -1,11 +1,11 @@
 module cell_centers_extended_m
   !! Define an abstraction for the collection of points used to compute gradidents:
   !! cell centers plus oundaries.
+  use gradient_m, only : gradient_t
   implicit none
 
   private
   public :: cell_centers_extended_t
-  public :: gradient_t
   public :: gradient_operator_t
   public :: scalar_1D_initializer_i
 
@@ -32,46 +32,6 @@ module cell_centers_extended_m
     double precision dx_
     type(mimetic_matrix_t) mimetic_matrix_
   end type
-
-  type gradient_t
-    !! Encapsulate gradient values produced only by .grad. (no other constructors)
-    private
-    double precision, allocatable :: vector_1D_(:) !! gradient values at cell faces (nodes in 1D)
-    double precision x_min_ !! domain lower boundary
-    double precision x_max_ !! domain upper boundary
-    integer cells_ !! number of grid cells spanning the domain
-  contains
-    procedure values
-    procedure faces
-  end type
-
-  interface gradient_t
-
-      pure module function construct_gradient(face_centered_values, x_min, x_max, cells) result(gradient)
-        !! Result is an object storing gradients at cell faces
-        implicit none
-        double precision, intent(in) :: face_centered_values(:), x_min, x_max
-        integer, intent(in) :: cells
-        type(gradient_t) gradient
-      end function
-
-  end interface
-
-  interface 
-
-    pure module function faces(self) result(x)
-      implicit none
-      class(gradient_t), intent(in) :: self
-      double precision, allocatable :: x(:)
-    end function
-
-     pure module function values(self) result(gradients)
-       implicit none
-       class(gradient_t), intent(in) :: self
-       double precision, allocatable :: gradients(:)
-     end function
-
-  end interface
 
   type cell_centers_extended_t
     !! Encapsulate information at cell centers and boundaries
--- src/fortran/mole_m.f90
+++ src/fortran/mole_m.f90
@@ -1,5 +1,6 @@
 module mole_m 
   !! MOLE Fortran public entities
-  use cell_centers_extended_m, only : cell_centers_extended_t, gradient_t, scalar_1D_initializer_i
+  use cell_centers_extended_m, only : cell_centers_extended_t, scalar_1D_initializer_i
+  use gradient_m, only : gradient_t
   implicit none
 end module mole_m
