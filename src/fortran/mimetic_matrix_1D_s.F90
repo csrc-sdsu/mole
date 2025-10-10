ac62a7fd99cfe3edfcc722b54e0562d6f409a261
Damian Rouson <rouson@lbl.gov>
2025-10-09 22:18:07 -0700

    refac(vector): rename matrix-vector RHS scalar_1D

--- src/fortran/mimetic_matrix_1D_s.F90
+++ src/fortran/mimetic_matrix_1D_s.F90
@@ -20,18 +20,18 @@ contains
     double precision, allocatable :: product_inner(:)
 
     associate(upper => size(self%upper_,1), lower => size(self%lower_,1))
-      associate(inner_rows => size(vector%scalar_1D_) - (upper + lower + 1))
+      associate(inner_rows => size(scalar_1D%scalar_1D_) - (upper + lower + 1))
 
         allocate(product_inner(inner_rows))
 
-        do concurrent(integer :: row = 1 : inner_rows) default(none) shared(product_inner, self, vector)
-          product_inner(row) = dot_product(self%inner_, vector%scalar_1D_(row + 1 : row + size(self%inner_)))
+        do concurrent(integer :: row = 1 : inner_rows) default(none) shared(product_inner, self, scalar_1D)
+          product_inner(row) = dot_product(self%inner_, scalar_1D%scalar_1D_(row + 1 : row + size(self%inner_)))
         end do
 
         matvec_product = [ &
-           matmul(self%upper_, vector%scalar_1D_(1 : size(self%upper_,2))) &
+           matmul(self%upper_, scalar_1D%scalar_1D_(1 : size(self%upper_,2))) &
           ,product_inner &
-          ,matmul(self%lower_, vector%scalar_1D_(size(vector%scalar_1D_) - size(self%lower_,2) + 1 : )) &
+          ,matmul(self%lower_, scalar_1D%scalar_1D_(size(scalar_1D%scalar_1D_) - size(self%lower_,2) + 1 : )) &
         ]
       end associate
     end associate
