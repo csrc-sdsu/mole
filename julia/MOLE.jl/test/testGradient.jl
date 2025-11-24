b357b7ef08db09285193f95c9057f39151f65854
Marc Loschen <marc.loschen@gmail.com>
2025-11-24 01:42:29 -0800

    created central MOLE file; small edits to test files

--- julia/MOLE.jl/test/testGradient.jl
+++ julia/MOLE.jl/test/testGradient.jl
@@ -1,12 +1,13 @@
 using Test, LinearAlgebra
 
-include("../src/gradient.jl")
+include("../src/MOLE.jl")
+using .MOLE
 
 tol = 1e-10
 
 @testset "Testing Gradient for order k=$k" for k=2:2:8
     m = 2*k+1
-    G = grad(k, m, 1/m)
+    G = MOLE.grad(k, m, 1/m)
     field = ones(m+2, 1)
     sol = G*field
     @test norm(sol) < tol
