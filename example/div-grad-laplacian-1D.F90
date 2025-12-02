384e17053cd5301ca8d840938e99e02d8a5823a3
Damian Rouson <rouson@lbl.gov>
2025-12-02 15:24:46 -0800

    refactor(example): redistrib macros, reform output

--- example/div-grad-laplacian-1D.F90
+++ example/div-grad-laplacian-1D.F90
@@ -6,17 +6,17 @@ contains
   pure function f(x)
     double precision, intent(in) :: x(:)
     double precision, allocatable :: f(:)
-    f = (x**3)/6
+    f = (x**3)/6 + (x**2)/2 + 1
   end function
 
   double precision elemental function df_dx(x)
     double precision, intent(in) :: x
-    df_dx = (x**2)/2
+    df_dx = (x**2)/2 + x
   end function
 
   double precision elemental function d2f_dx2(x)
     double precision, intent(in) :: x
-    d2f_dx2 = x
+    d2f_dx2 = x + 1
   end function
 
 end module functions_m
@@ -31,8 +31,6 @@ program div_grad_laplacian_1D
   implicit none
 
   procedure(scalar_1D_initializer_i), pointer :: scalar_1D_initializer => f
-
-  print *,"Grid        Expected Values    Actual Values"
 
   print *,new_line('')
   print *,"            2nd-order approximations"
@@ -49,61 +47,51 @@ program div_grad_laplacian_1D
 contains
 
   subroutine output(order)
-    use iso_fortran_env, only : output_unit
     integer, intent(in) :: order
   
-#ifndef __GFORTRAN__
+#ifdef __GFORTRAN__
+    type(scalar_1D_t) s
+    type(vector_1D_t) grad_s
+    type(laplacian_1D_t) laplacian_s
+    type(file_t) s_table, grad_s_table, laplacian_s_table
+
+    s           = scalar_1D_t(scalar_1D_initializer, order=order, cells=20, x_min=0D0, x_max=40D0)
+    grad_s      = .grad. s
+    laplacian_s = .laplacian. s
+#else
     associate(   s           => scalar_1D_t(scalar_1D_initializer, order=order, cells=20, x_min=0D0, x_max=40D0))
       associate( grad_s      => .grad. s      &
                 ,laplacian_s => .laplacian. s &
       )
+#endif
         associate( s_grid           => s%grid() &
                   ,grad_s_grid      => grad_s%grid() &
                   ,laplacian_s_grid => laplacian_s%grid() &
         )
-          associate( plot => gnuplot(string_t([character(len=15)::"x", "f(x)"         , "f(x)"         ]), s_grid, f(s_grid), s%values()))
-             call plot%write_lines()
+#ifndef __GFORTRAN__
+          associate( &
+             s_table => tabulate(string_t([character(len=18)::"x", "f(x) exp"         , "f(x) act"         ]), s_grid, f(s_grid), s%values()) &
+            ,grad_s_table => tabulate(string_t([character(len=18)::"x", ".grad. f exp"     , ".grad. f act"     ]), grad_s_grid, df_dx(grad_s_grid), grad_s%values()) &
+            ,laplacian_s_table => tabulate(string_t([character(len=18)::"x", ".laplacian. f exp", ".laplacian. f act"]), laplacian_s_grid, d2f_dx2(laplacian_s_grid), laplacian_s%values()) &
+          )
+#else
+             s_table = tabulate(string_t([character(len=18)::"x", "f(x) exp."         , "f(x) act."         ]), s_grid, f(s_grid), s%values())
+             grad_s_table = tabulate(string_t([character(len=18)::"x", ".grad. f exp."     , ".grad. f act."     ]), grad_s_grid, df_dx(grad_s_grid), grad_s%values())
+             laplacian_s_table = tabulate(string_t([character(len=18)::"x", ".laplacian. f exp.", ".laplacian. f act."]), laplacian_s_grid, d2f_dx2(laplacian_s_grid), laplacian_s%values())
+#endif
+             call s_table%write_lines()
+             call grad_s_table%write_lines()
+             call laplacian_s_table%write_lines()
           end associate
-          associate( plot => gnuplot(string_t([character(len=15)::"x", ".grad. f"     , ".grad. f"     ]), grad_s_grid, df_dx(grad_s_grid), grad_s%values()))
-             call plot%write_lines()
-          end associate
-
-          associate( plot => gnuplot(string_t([character(len=15)::"x", ".laplacian. f", ".laplacian. f"]), laplacian_s_grid, d2f_dx2(laplacian_s_grid), laplacian_s%values()))
-             call plot%write_lines()
-          end associate
+#ifndef __GFORTRAN__
         end associate
       end associate
     end associate
-#else
-    block
-      type(scalar_1D_t) s
-      type(vector_1D_t) grad_s
-      type(laplacian_1D_t) laplacian_s
-      type(file_t) plot
-
-      s           = scalar_1D_t(scalar_1D_initializer, order=order, cells=20, x_min=0D0, x_max=40D0)
-      grad_s      = .grad. s
-      laplacian_s = .laplacian. s
-       
-      associate( &
-         s_grid           => s%grid() &
-        ,grad_s_grid      => grad_s%grid() &
-        ,laplacian_s_grid => laplacian_s%grid() &
-      )
-        plot = gnuplot(string_t([character(len=15)::"x", "f(x)"         , "f(x)"         ]), s_grid, f(s_grid), s%values())
-        call plot%write_lines()
-        plot = gnuplot(string_t([character(len=15)::"x", ".grad. f"     , ".grad. f"     ]), grad_s_grid, df_dx(grad_s_grid), grad_s%values())
-        call plot%write_lines()
-        plot = gnuplot(string_t([character(len=15)::"x", ".laplacian. f", ".laplacian. f"]), laplacian_s_grid, d2f_dx2(laplacian_s_grid), laplacian_s%values())
-        call plot%write_lines()
-     end associate
-    end block
 #endif
-
   end subroutine
 
 
-  pure function gnuplot(headings, abscissa, expected, actual) result(file)
+  pure function tabulate(headings, abscissa, expected, actual) result(file)
     double precision, intent(in), dimension(:) :: abscissa, expected, actual
     type(string_t), intent(in) :: headings(:)
     type(file_t) file
@@ -112,8 +100,8 @@ contains
     file = file_t([ &
        string_t("") &
       ,headings .separatedBy. "  " &
-      ,string_t("------------------------------------------------") &
-      ,[( string_t(abscissa(line)) // "   " // string_t(expected(line)) // "   " // string_t(actual(line)), line = 1, size(abscissa))] &
+      ,string_t("----------------------------------------------------------") &
+      ,[( string_t(abscissa(line)) // "      " // string_t(expected(line)) // "        " // string_t(actual(line)), line = 1, size(abscissa))] &
     ])
   end function
 
