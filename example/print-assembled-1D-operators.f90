42bc326952197e7e738e5803b4de0a25fd419de2
Damian Rouson <rouson@lbl.gov>
2025-12-05 09:55:33 -0800

    feat(example): add command-line flags
    Usage:
      fpm run \
      --example print-assembled-1D-operators \
      --compiler flang-new \
      --flag "-O3" \
      -- [--help|-h] | [--grad]  [--div] [--order <integer>]
    
    where square brackets indicate optional arguments and angular
    brackets indicate user input values.

--- example/print-assembled-1D-operators.f90
+++ example/print-assembled-1D-operators.f90
@@ -12,65 +12,67 @@ program print_assembled_1D_operators
   associate( &
      gradient   => command_line%argument_present(["--grad" ]) &
     ,divergence => command_line%argument_present(["--div"  ]) &
-    ,order      => command_line%argument_present(["--order"]) &
+    ,order      => command_line%flag_value("--order") &
   )
 
-  if (command_line%argument_present([character(len=len("--help")) :: ("--help"), "-h"])) then
-    stop                                 new_line('') // new_line('') &
-      // 'Usage:'                                     // new_line('') &
-      // '  fpm run \'                                // new_line('') &
-      // '  --example print-assembled-1D-operators \' // new_line('') &
-      // '  --compiler flang-new \'                   // new_line('') &
-      // '  --flag "-O3" \'                           // new_line('')
-  end if
+    if (command_line%argument_present([character(len=len("--help")) :: ("--help"), "-h"])) then
+      stop                                 new_line('') // new_line('') &
+        // 'Usage:'                                     // new_line('') &
+        // '  fpm run \'                                // new_line('') &
+        // '  --example print-assembled-1D-operators \' // new_line('') &
+        // '  --compiler flang-new \'                   // new_line('') &
+        // '  --flag "-O3" \'                           // new_line('') & 
+        // '  -- [--help|-h] | [--grad]  [--div] [--order <integer>]' // new_line('') // new_line('') &
+        // 'where square brackets indicate optional arguments and angular brackets indicate user input values.' // new_line('')
+    end if
 
-  print *, new_line(""), "Gradient operator (2nd order, dx=1, 5 cells)"
+    default_usage: &
+    associate(print_all => .not. any([gradient, divergence, len(order)/=0]))
 
-  associate(grad_2nd_order => gradient_operator_1D_t(k=2, dx=1D0, cells=5))
-    associate(G => grad_2nd_order%assemble())
-      do row = 1, size(G,1)
-        associate(csv_row => .csv. string_t(G(row,:)))
-          print '(a)', csv_row%string()
-        end associate
-      end do
-    end associate
-  end associate
+      if (print_all .or. (gradient   .and. len(order)==0) .or. (gradient   .and. order=="2")) call print_gradient_operator(  k=2, dx=1D0, m=5)
+      if (print_all .or. (divergence .and. len(order)==0) .or. (divergence .and. order=="2")) call print_divergence_operator(k=2, dx=1D0, m=5)
+      if (print_all .or. (gradient   .and. len(order)==0) .or. (gradient   .and. order=="4")) call print_gradient_operator(  k=4, dx=1D0, m=9)
+      if (print_all .or. (divergence .and. len(order)==0) .or. (divergence .and. order=="4")) call print_divergence_operator(k=4, dx=1D0, m=9)
 
-  print *, new_line(""), "Divergence operator (2nd order, dx=1, 5 cells)"
-  
-  associate(div_2nd_order => divergence_operator_1D_t(k=2, dx=1D0, cells=5))
-    associate(D => div_2nd_order%assemble())
-      do row = 1, size(D,1)
-        associate(csv_row => .csv. string_t(D(row,:)))
-          print '(a)', csv_row%string()
-        end associate
-      end do
-    end associate
-  end associate
-
-  print *, new_line(""), "Gradient operator (4th order, dx=1, 9 cells)"
-
-  associate(grad_4th_order => gradient_operator_1D_t(k=4, dx=1D0, cells=9))
-    associate(G => grad_4th_order%assemble())
-      do row = 1, size(G,1)
-        associate(csv_row => .csv. string_t(G(row,:)))
-          print '(a)', csv_row%string()
-        end associate
-      end do
-    end associate
-  end associate
-
-  print *, new_line(""), "Divergence operator (4th order, dx=1, 9 cells)"
-
-  associate(div_op => divergence_operator_1D_t(k=4, dx=1D0, cells=9))
-    associate(D => div_op%assemble())
-      do row = 1, size(D,1)
-        associate(csv_row => .csv. string_t(D(row,:)))
-          print '(a)', csv_row%string()
-        end associate
-      end do
-    end associate
-  end associate
+    end associate default_usage
   end associate command_line_settings
+
+contains
+
+  subroutine print_gradient_operator(k, dx, m)
+    integer, intent(in) :: k, m 
+    double precision, intent(in) :: dx
+
+    print *, new_line(""), "Gradient operator: order = ", k, " | cells = ", m, " | dx = ", dx
+
+    associate(grad_op => gradient_operator_1D_t(k, dx, cells=m))
+      associate(G => grad_op%assemble())
+        do row = 1, size(G,1)
+          associate(csv_row => .csv. string_t(G(row,:)))
+            print '(a)', csv_row%string()
+          end associate
+        end do
+      end associate
+    end associate
+
+  end subroutine
+
+  subroutine print_divergence_operator(k, dx, m)
+    integer, intent(in) :: k, m 
+    double precision, intent(in) :: dx
+
+    print *, new_line(""), "Divergence operator: order = ", k, " | cells = ", m, " | dx = ", dx
+
+    associate(div_op => divergence_operator_1D_t(k, dx, cells=m))
+      associate(D => div_op%assemble())
+        do row = 1, size(D,1)
+          associate(csv_row => .csv. string_t(D(row,:)))
+            print '(a)', csv_row%string()
+          end associate
+        end do
+      end associate
+    end associate
+
+  end subroutine
 
 end program
