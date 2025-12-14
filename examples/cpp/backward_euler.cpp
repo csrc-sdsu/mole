5f55bf31d9d015a1c1d73ec79268dcfd13ba48b9
Kristy Le <kle2302@sdsu.edu>
2025-12-14 08:22:07 -0800

    Modified iter to max_iter and reformatted the header.

--- examples/cpp/backward_euler.cpp
+++ examples/cpp/backward_euler.cpp
@@ -1,7 +1,12 @@
 /**
  * @file backward_euler.cpp
  * @brief Solves a first-order ODE with backward Euler with an initial value
- * condition. Equation: $\frac{dy}{dt} = \sin^2(t) \cdot y$ Domain: N/A Boundary
+ * condition.
+ *
+ * Equation: dy/dt = sin^2(t) * y
+ *
+ * Domain: N/A Boundary
+ *
  * Conditions: N/A
  */
 
@@ -16,8 +21,8 @@ int main() {
   constexpr double h = 0.01;             // Step-size h
   arma::vec t = arma::regspace(0, h, 5); // Calculates up to y(5) at step-size h
   arma::mat y = arma::mat(1, t.size(), arma::fill::zeros);
-  constexpr double tol = 1e-6; // Tolerance for fixed-point iteration
-  constexpr int iter = 100;    // Max iterations for fixed-point iteration
+  constexpr double tol = 1e-6;  // Tolerance for fixed-point iteration
+  constexpr int max_iter = 100; // Max iterations for fixed-point iteration
 
   // Initial conditions
   y(0) = 2.0;
@@ -25,9 +30,10 @@ int main() {
   // Backward Euler
   for (int i = 0; i < t.size() - 1; i++) {
     double y_old = y(i);
-    double xn = y_old;               // Initial input for fixed-point iteration
-    double xnp1;                     // x at iteration n+1
-    for (int n = 0; n < iter; n++) { // fixed-point iteration for rootfinding
+    double xn = y_old; // Initial input for fixed-point iteration
+    double xnp1;       // x at iteration n+1
+    for (int n = 0; n < max_iter;
+         n++) { // fixed-point iteration for rootfinding
       xnp1 = y_old + h * f(t(i + 1), xn); // Backward Euler
       if (std::abs(xnp1 - xn) <
           tol) { // Stopping criteria for approximate relative error
