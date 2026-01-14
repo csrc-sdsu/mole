d847a9a4424ccf9cc995609783a6625bbb17ad90
Marc Loschen <marc.loschen@gmail.com>
2026-01-13 20:10:24 -0800

    Added copyright language to source files

--- julia/MOLE.jl/src/gradient.jl
+++ julia/MOLE.jl/src/gradient.jl
@@ -1,3 +1,9 @@
+#=
+    SPDX-License-Identifier: GPL-3.0-or-later
+    © 2008-2024 San Diego State University Research Foundation (SDSURF).
+    See LICENSE file or https://www.gnu.org/licenses/gpl-3.0.html for details.
+=#
+
 """
     grad(k, m, dx)
 
