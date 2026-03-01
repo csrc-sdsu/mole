c9cb5443fa9a3e6db1968a0ec4f38bf3d09e442b
Valeria Barra <valeriabarra21@gmail.com>
2026-03-01 10:33:20 -0800

    Better namespace management using import

--- julia/MOLE.jl/test/Operators/laplacian.jl
+++ julia/MOLE.jl/test/Operators/laplacian.jl
@@ -2,7 +2,7 @@ tol = 1e-10
 
 @testset "Testing laplacian for order k=$k" for k=2:2:8
     m = 2*k+1
-    L = MOLE.lap(k, m, 1/m)
+    L = Operators.lap(k, m, 1/m)
     field = ones(m+2, 1)
     sol = L*field
     @test norm(sol) < tol
