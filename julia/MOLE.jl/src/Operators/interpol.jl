426552d46d2eaa0943d879b6346438e3f313a498
Jose Luis Baranda <bluisjose96@gmail.com>
2026-04-22 11:51:11 -0700

    Adding interpol.jl, hyperbolic1D.jl and edit on Operators.jl

---
+++ julia/MOLE.jl/src/Operators/interpol.jl
@@ -0,0 +1,40 @@
+using SparseArrays
+#=
+        SPDX-License-Identifier: GPL-3.0-or-later
+        © 2008-2024 San Diego State University Research Foundation (SDSURF).
+        See LICENSE file or https://www.gnu.org/licenses/gpl-3.0.html for details.
+=#
+
+function interpol(m::Int,c::Float64)
+    """
+    implementation of MATLAB interpol.m operator for Julia
+    INPUTS: 
+       'm::Int'   : number of cells
+       'c::Float' : left interpolation coefficient
+    OUTPUTS:
+       I : a (m+1)×(m+2) one-dimensional interpolator of 2nd-order
+    """
+
+    #Assertions:
+    @assert m>=4 ["m >= 4"];
+    @assert c >= 0 && c <= 1 ["0 <= c <= 1"];
+
+    #Dimensions of I:
+    n_rows = m+1;
+    n_cols = m+2;
+
+    I = spzeros(n_rows, n_cols);
+
+    I[1,1] = 1;
+    I[end,end]=1;
+
+    #Average between two continuous cells
+    avg = [c 1-c];
+
+    j = 2;
+    for i = 2: n_rows - 1
+        I[i, j:j+2-1] = avg;
+        j = j + 1;
+    end
+end
+
