5bcc7f98a042b6dee024d0cd8a77e547e45f4a3b
Miguel Dumett <mdumett@sdsu.edu>
2026-05-18 17:53:32 -0700

    consistent matrix curl2D
    This curl2D code is consistent with was before without using vector field data. It output a matrix. A test is also added.

--- src/matlab_octave/curl2D.m
+++ src/matlab_octave/curl2D.m
@@ -1,62 +1,43 @@
-function C = curl2D(k, m, dx, n, dy, west, east, south, north, U, V)
-% PURPOSE
-% Returns a two-dimensional mimetic curl operator
+% SPDX-License-Identifier: GPL-3.0-only
+% 
+% Copyright 2008-2024 San Diego State University Research Foundation (SDSURF).
 %
-% DESCRIPTION
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, version 3.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% LICENSE file or on the web GNU General Public License 
+% <https://www.gnu.org/licenses/> for more details.
+%
+% ------------------------------------------------------------------------
+function C = curl2D(k, m, dx, n, dy)
+% Returns a two-dimensional mimetic curl operator
+% The 1st component is computed at horizontal faces (tangential vertical derivative)
+% The 2nd component is computed at vertical faces (tangential horizontal derivative)
+% The (scalar) curl or third component is computed at the cell centers (normal to the plane)
+%
 % Parameters:
 %                k : Order of accuracy
 %                m : Number of cells along x-axis
 %               dx : Step size along x-axis
 %                n : Number of cells along y-axis
 %               dy : Step size along y-axis
-%             west : west limit on x-axis
-%             east : east limit on x-axis
-%            south : south limit on y-axis
-%            north : north limit on Y-axis
-%                U : Vector space function acting on x-direction
-%                    U(X,Y) must be defined as function handle
-%                V : Vector space function acting on y-direction
-%                    V(X,Y) must be defined as function handle
-%
-% SYNTAX
-% C = curl2D(k, m, dx, n, dy, west, east, south, north, U, V)
-%
-% ----------------------------------------------------------------------------
-% SPDX-License-Identifier: GPL-3.0-or-later
-% © 2008-2024 San Diego State University Research Foundation (SDSURF).
-% See LICENSE file or https://www.gnu.org/licenses/gpl-3.0.html for details.
-% ----------------------------------------------------------------------------
 
+    C = sparse(n*(m+1)+(n+1)*m+n*m, (n+1)*m+n*(m+1)+(n+1)*(m+1));
 
-% Arguments validation
-    assert(isa(U, 'function_handle') && isa(V, 'function_handle'), ...
-           'U and V must be defined as function handle');
-    assert(nargin(U) == 2 && nargin(V) == 2, ...
-           'U and V must be defined as 2D vector functions');
-    assert(west < east && south < north , 'west < east, south < north');
+    Dx = full(div(k, m, dx));
+    Dx = sparse(Dx(2:end-1,:));
+    Dy = full(div(k, n, dy));
+    Dy = sparse(Dy(2:end-1,:));
 
-% Variables initialization
-    F = sparse(2*m*n+m+n, 1);
-    xaxis = west : (dx/2) : east;
-    yaxis = south : (dy/2) : north;
-
-% Linearizing F as function of U and V: F = V_x - U_y
-    f = 1;
-    for j = 2 : 2 : 2*n+1
-        for i = 1 : 2 : 2*m+1
-            F(f) = V(xaxis(i), yaxis(j));  % F in the x direction (V_x)
-            f = f + 1;
-        end
-    end
-
-    for j = 1 : 2 : 2*n+1
-        for i = 2 : 2 : 2*m+1
-            F(f) = -U(xaxis(i), yaxis(j));  % F in the y direction (-U_y)
-            f = f + 1;
-        end
-    end
-
-    C = div2D(k, m, dx, n, dy)*F;  % Mimetic curl as C = Div2D*F
-    C = reshape(C, m+2, n+2);      % Reshaping C as matrix
-    C = C(2:end-1, 2:end-1);       % Removing boundaries
+    % first component
+    C(1:(m+1)*n,(n+1)*m+n*(m+1)+1:end) = kron(Dy,speye(m+1,m+1));
+    % second component
+    C((m+1)*n+1:n*(m+1)+(n+1)*m,(n+1)*m+n*(m+1)+1:end) = - kron(speye(n+1,n+1),Dx);
+    % third component
+    C(m*(n+1)+(m+1)*n+1:end,1:m*(n+1)) = - kron(Dy,speye(m,m));
+    C(m*(n+1)+(m+1)*n+1:end,m*(n+1)+1:m*(n+1)+n*(m+1)) = kron(speye(n,n),Dx);
 end
