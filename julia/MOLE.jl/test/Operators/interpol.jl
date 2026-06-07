ef4ea7361cc54ac811d600628f669573374d9272
Jose Luis Baranda <bluisjose96@gmail.com>
2026-06-07 13:06:37 -0700

    removed comment of tests that were used to test how I would create for loop for testset

--- julia/MOLE.jl/test/Operators/interpol.jl
+++ julia/MOLE.jl/test/Operators/interpol.jl
@@ -1,32 +1,4 @@
 tol = 1e-10
-#=
-@testset "Testing interpolating operator for m=5 and c=0.5" begin
-    m = 5
-    c = 0.5
-    I = Operators.interpol(m,c)
-    A = [1 0 0 0 0 0 0;
-     0 0.5 0.5 0 0 0 0;
-     0 0 0.5 0.5 0 0 0;
-     0 0 0 0.5 0.5 0 0;
-     0 0 0 0 0.5 0.5 0;
-     0 0 0 0 0 0 1]
-@test norm(I-A) < tol
-end
-
-
-@testset "Testing interpolating operator for m=5 and c=1" begin
-    m = 5
-    c = 1.0
-    I = Operators.interpol(m,c)
-    A = [1 0 0 0 0 0 0;
-         0 1 0 0 0 0 0;
-         0 0 1 0 0 0 0;
-         0 0 0 1 0 0 0;
-         0 0 0 0 1 0 0;
-         0 0 0 0 0 0 1]
-@test norm(I-A) < tol
-end
-=#
 
 @testset "Testing interpolator operator for m=5 and c=$c" for c=0:0.25:1
     m = 5
