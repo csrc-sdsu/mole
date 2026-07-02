44a6a3d3736dba9ee2b2ed8f480533f4f01d2476
jbrzensk <jbrzensk@gmail.com>
2026-07-02 09:55:51 -0700

    add full explanantion

--- src/matlab_octave/addScalarBC1D.m
+++ src/matlab_octave/addScalarBC1D.m
@@ -1,8 +1,52 @@
 function [A, b] = addScalarBC1D(A, b, k, m, dx, dc, nc, v)
 % PURPOSE
-% Separates cases non-periodic and periodic for dealing with boundary data
+% This function assumes that the unknown u, which represents the discrete
+% solution the continuous second-order 1D PDE operator 
+%                                   L U = f, 
+% with continuous boundary condition 
+%                              a0 U + b0 dU/dn = g,
+% are given at the 1D cell centers and boundary face centers. Furthermore,
+% all discrete calculations are performed at the 1D cell centers and boundary 
+% face centers.
 %
 % DESCRIPTION
+% The function receives as input quantities associated to the discrete
+% analog of the continuous problem given by the squared linear system 
+%                                 A u = b 
+% where A is the discrete analog of L and b is the discrete analog of g,
+% both constructed by the user without boundary conditions.
+% The function output is the modified square linear system 
+%                                 A u = b
+% where both A and b include boundary condition information.
+%
+% The boundary condition is always one of the following forms:
+%
+% For Dirichlet set: a0 not equal zero and b0 = 0.
+% For Neumann set  : a0 = 0 and b0 not equal zero.
+% For Robin set    : both a0 and b0 not equal zero.
+% For Periodic set : both a0 = 0 and b0 = 0.
+%
+% For periodic bc, it is assumed that not only u but also du/dn are the 
+% same in both extremes of the domain since a second-order PDE is assumed.
+%
+% Periodic boundary conditions can be applied along some axes and
+% non-periodic to some others.
+% 
+% For consistence with the way boundary operators are calculated to avoid 
+% overwriting of the values v, the left and right boundary conditions are
+% assumed to be column vectors of (m+2)*n components, and the bottom and 
+% top faces are assumed to be vectors of (m+2)*(n+2) components. 
+%
+% The order of these components is as follows:
+% For left and right edges, the ordering is the one given by columns 
+% vectors where x increases. For bottom and top faces, the ordering is the 
+% one given by columns vectors where y increases.
+%
+% The code assumes the following assertions:
+% assert(k >= 2, 'k >= 2');
+% assert(mod(k, 2) == 0, 'k % 2 = 0');
+% assert(m >= 2*k+1, ['m >= ' num2str(2*k+1) ' for k = ' num2str(k)]);
+%
 % Parameters:
 % output
 %        A0 : Linear operator with boundary conditions added
