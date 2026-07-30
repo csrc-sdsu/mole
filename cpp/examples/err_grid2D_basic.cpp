e49f00e8570d04bd2b28b08559e6663dc28f674c
Tony-Drummond <ldrummondlewis@sdsu.edu>
2026-07-29 20:54:59 -0700

    Added basic examples and CMakeLists.txt files

---
+++ cpp/examples/err_grid2D_basic.cpp
@@ -0,0 +1,47 @@
+// This example generates some grid issues
+#include "MOLE_grids.h"
+#include <iostream>
+
+int main() {
+    gridParams2D p;
+    p.topology = 'x';
+    p.m = 3; p.n = 3;
+    p.dx = 1.0; p.dy = 1.0;
+
+    // This grid has an invalid topology
+    grid2D g(p);
+    if (!g.validGrid()) {
+        g.print_ErrorLog();
+    } else {
+        std::cout << "grid2D built OK, nodes_X is "
+        << g.grid.nodes_X.rows() << " x " << g.grid.nodes_X.cols()
+        << "\n";
+    }
+    
+
+    // This second grid has an invalid dx
+    p.topology = 'u';
+    p.dx = 0;
+    grid2D g1(p);
+     if (!g1.validGrid()) {
+        g1.print_ErrorLog();
+    } else {
+        std::cout << "grid2D built OK, nodes_X is "
+        << g1.grid.nodes_X.rows() << " x " << g1.grid.nodes_X.cols()
+        << "\n";
+    } 
+    
+    // This third attempt the grid has valid parameters
+    p.topology = 'u';
+    p.dx = 1.0;
+    grid2D g2(p);
+     if (!g2.validGrid()) {
+        g2.print_ErrorLog();
+    } else {
+        std::cout << "grid2D built OK, nodes_X is "
+        << g2.grid.nodes_X.rows() << " x " << g2.grid.nodes_X.cols()
+        << "\n";
+    } 
+
+    return 0;
+}
