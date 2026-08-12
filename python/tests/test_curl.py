b876a0c6ee729dc79ca8f7d877ebb01d67063f75
Christopher Paolini <paolini@engineering.sdsu.edu>
2026-08-11 22:04:09 -0700

    Update pyproject.toml

--- python/tests/test_curl.py
+++ python/tests/test_curl.py
@@ -3,7 +3,7 @@ import pytest
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+#sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from pymole import Curl, Divergence, Grid
 
 class TestCurl:
