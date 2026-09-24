3027ed9f4945c4f966059d980482147f3015a145
Christopher Paolini <paolini@engineering.sdsu.edu>
2026-09-24 14:52:03 -0700

    feat: add pymole Python library with mimetic operators, documentation, and examples

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -30,4 +30,4 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output ------------------------------------------------
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
